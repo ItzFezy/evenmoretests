0d8384ce4ae0ffcb1b9546edf3cc61c97da251ac
ItzFezy <148616668+ItzFezy@users.noreply.github.com>
2024-05-09 18:46:40 -0500

    Add files via upload

---
+++ src/main/java/plugins/customconfigapi/ECA.java
@@ -0,0 +1,14 @@
+package plugins.customconfigapi;
+
+import org.bukkit.plugin.Plugin;
+
+public class ECA {
+    private static Plugin plugin;
+    public static void setPlugin(Plugin plugin){
+        ECA.plugin = plugin;
+    }
+
+    public static Plugin getPlugin() {
+        return plugin;
+    }
+}
